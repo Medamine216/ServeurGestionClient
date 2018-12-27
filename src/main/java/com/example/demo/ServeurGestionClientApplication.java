ef7d18b37ec0d0e009ee11171429add0fe3d73e5
Medamine216 <info.ds512013@gmail.com>
2018-12-27 11:00:51 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeurGestionClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionClientApplication.class, args);
+	}
+
+}
+
